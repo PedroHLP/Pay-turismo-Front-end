73e3c3c136e4e9e3766bb59b6360b7c751fd0ef7
Lucas <lucasgabriel@allinformatica.com.br>
2024-06-05 16:02:30 -0300

    feat: add necessary fields to register a new user

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,9 +1,9 @@
-import { useRef, useState, useEffect } from "react";
-import useAuth from "../hooks/useAuth";
-import { FaCheck, FaCheckCircle, FaCheckDouble } from "react-icons/fa";
-import { useNavigate, useLocation } from "react-router-dom";
-import Header from "../components/Header";
-import { Container, Form, Button, InputGroup, Row } from "react-bootstrap";
+import { Container, Form, Button,  Row, Col, FloatingLabel, Image, Tab, Tabs } from "react-bootstrap";
+
+import image from '../assets/sign_up.svg';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import InputMask from 'react-input-mask';
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
@@ -11,123 +11,210 @@ const REGISTER_URL = '/users/register';
 
 
 const Register = () => {
-    const errRef = useRef();
 
-    const { auth } = useAuth();
+    const basicInfoTab = (
+        <Tab eventKey="basicInfo" title="Informações Básicas">
+            <Form.Group className="mb-3">
+                <FloatingLabel label="CPF">
+                <Form.Control
+                    as={InputMask}
+                    mask="999.999.999-99"
+                    type="text"
+                    id="cpf"
+                    autoComplete='off'
+                    placeholder="CPF"
+                    required
+                />
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="Nome">
+                    <Form.Control type="text" id="username" autoComplete='off'
+                    placeholder="Nome" required/>
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="Email">
+                    <Form.Control 
+                        type="email"
+                        id="email"
+                        autoComplete='off'
+                        placeholder="Email"
+                        required
+                    />
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="Celular">
+                    <Form.Control 
+                        as={InputMask}
+                        mask="(99) 99999-9999"
+                        type="text"
+                        id="phone"
+                        autoComplete='off'
+                        placeholder="Celular"
+                        required
+                    />
+                </FloatingLabel>
+            </Form.Group>
+        </Tab>
+    )
 
-    const [user, setUser] = useState('');
-    const [validName, setValidName] = useState(false);
+    const addressTab = (
+        <Tab eventKey="address" title="Endereço">
+            <Form.Group className="mb-3">
+                <FloatingLabel label="CEP">
+                    <Form.Control 
+                        as={InputMask}
+                        mask="99999-999"
+                        type="text"
+                        id="cep"
+                        autoComplete='off'
+                        placeholder="CEP"
+                        required
+                    />
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="Endereço">
+                    <Form.Control
+                        type="text"
+                        id="address"
+                        autoComplete='off'
+                        placeholder="Endereço"
+                        required
+                    />
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="Número">
+                    <Form.Control type="number" id="number" autoComplete='off'
+                    placeholder="Number" required/>
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="Complemento">
+                    <Form.Control
+                        type="text"
+                        id="complement"
+                        autoComplete='off'
+                        placeholder="Complemento"
+                        required
+                    />
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="Cidade">
+                    <Form.Control
+                        type="text"
+                        id="city"
+                        autoComplete='off'
+                        placeholder="Cidade"
+                        required
+                    />
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="Estado">
+                    <Form.Control
+                        type="text"
+                        id="state"
+                        autoComplete='off'
+                        placeholder="Estado"
+                        required
+                    />
+                </FloatingLabel>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <FloatingLabel label="País">
+                    <Form.Control
+                        type="text"
+                        id="country"
+                        autoComplete='off'
+                        placeholder="País"
+                        required
+                    />
+                </FloatingLabel>
+            </Form.Group>
+        </Tab>
+    )
 
-    const [pwd, setPwd] = useState('');
-    const [validPwd, setValidPwd] = useState(false);
-
-    const [matchPwd, setMatchPwd] = useState('');
-    const [validMatch, setValidMatch] = useState(false);
-
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
-
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    // checa se o usuário é valido toda vez que o valor de user altera
-    useEffect(() => {
-        const isUserValid = USER_REGEX.test(user);
-        setValidName(isUserValid);
-    }, [user])
-
-    // valida as senhas
-    useEffect(() => {
-        const isPasswordValid = PWD_REGEX.test(pwd);
-        setValidPwd(isPasswordValid);
-        
-        let isPasswordMatching = matchPwd === pwd;
-        
-        if (!isPasswordValid) 
-            isPasswordMatching = false;
-        
-        setValidMatch(isPasswordMatching);
-    }, [pwd, matchPwd])
-
-    // limpa a mensagem de erro ao enviar form se validar uma das condições
-    useEffect(() => {
-        setErrMsg('');
-    }, [validName, validPwd, validMatch])
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        // por mais que o botão esteja bloqueado, verificar se tudo bate
-        if (!validName || !validPwd || !validMatch)
-            return;
-
-        setSuccess(true);
-    }
+    const documentsTab = (
+        <Tab eventKey="documents" title="Documentos">
+            <Form.Group className="mb-3">
+                <Form.Label>Foto com documento</Form.Label>
+                <Form.Control
+                    type="file"
+                    id="documentPhoto"
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Selfie com identificação</Form.Label>
+                <Form.Control
+                    type="file"
+                    id="selfie"
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Comprovante de residência</Form.Label>
+                <Form.Control
+                    type="file"
+                    id="residenceProof"
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Comprovante do Cadastur</Form.Label>
+                <Form.Control
+                    type="file"
+                    id="cadasturProof"
+                    required
+                />
+            </Form.Group>
+        </Tab>
+    )
 
     return (
         <>
-        <Container fluid>
-            <Row className="justify-content-center">
-                <div className="col-md-8">
-                    <div className="px-5 py-3 border border-secondary bg-light">
-                        {success ? (
-                            <div className="text-center text-success">
-                                <FaCheckCircle size={50}/>
-                                <h5 className="mt-2">Cadastro realizado com sucesso!</h5>
-                            </div>
-                        ) : (
-                            <Form onSubmit={handleSubmit}>
-                                <div className="text-center">
-                                    <h3>Novo usuário</h3>
-                                    <hr/>
-                                </div>
-                                <p className="text-center text-danger" aria-live="assertive">{errMsg}</p>
-                                <Form.Group className="mb-3">
-                                    <Form.Label htmlFor="user">Usuário:</Form.Label>
-                                    <InputGroup>
-                                        <Form.Control type="text" id="name" onChange={(e) => setUser(e.target.value)} required/>
-                                        <InputGroup.Text className={validName ? "text-success" : "visually-hidden"}>
-                                            <FaCheck/>
-                                        </InputGroup.Text>
-                                    </InputGroup>
-                                    <Form.Text>
-                                        Deve conter 4 a 24 caracteres, começar com uma letra, podendo conter letras, números, underscores e/ou ífens. Não pode conter caracteres especiais.
-                                    </Form.Text>
-                                </Form.Group>
-                                <Form.Group className="mb-3">
-                                    <Form.Label htmlFor="password">Senha:</Form.Label>
-                                    <InputGroup>
-                                        <Form.Control type="password" id="password" onChange={(e) => setPwd(e.target.value)} required/>
-                                        <InputGroup.Text className={validPwd ? "text-success" : "visually-hidden"}>
-                                            <FaCheck/>
-                                        </InputGroup.Text>
-                                    </InputGroup>
-                                    <Form.Text>
-                                        Deve conter de 8 a 24 caracteres, letras maiúsculas e minúsculas, um número e um caracter especial ao menos.<br />
-                                        Caracteres especiais permitidos:
-                                        <span className="ms-1">!</span>
-                                        <span className="ms-1">@</span>
-                                        <span className="ms-1">#</span>
-                                        <span className="ms-1">$</span>
-                                        <span className="ms-1">%</span>
-                                    </Form.Text>
-                                </Form.Group>
-                                <Form.Group className="mb-3">
-                                    <Form.Label htmlFor="confirm">Confirme a senha:</Form.Label>
-                                    <InputGroup>
-                                        <Form.Control type="password" id="confirm" onChange={(e) => setMatchPwd(e.target.value)} required/>
-                                        <InputGroup.Text className={validMatch ? "text-success" : "visually-hidden"}>
-                                            <FaCheckDouble/>
-                                        </InputGroup.Text>
-                                    </InputGroup>
-                                </Form.Group>
-                                <Button type="submit" className="my-3 w-100" disabled={validName && validPwd && validMatch ? '' : 'disabled'}>Cadastrar</Button>
-                            </Form>
-                        )}
-                    </div>
-                </div>
-            </Row>
-        </Container>
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <Container>
+                <Row className="align-items-center" xs={1} md={2}>
+                    <Col className="py-4">
+                        <div className="text-center"><Image src={image} width={300}/></div>
+                    </Col>
+                    <Col>
+                        <Form>
+                            <Tabs className="mb-3" fill>
+                                {basicInfoTab}
+                                {addressTab}
+                                {documentsTab}
+                            </Tabs>
+                            {/* <Row xs={2}>
+                                <Col>
+                                    <Button
+                                    className="mb-3 w-100"
+                                    variant="secondary"
+                                    >
+                                    Anterior
+                                    </Button>
+                                </Col>
+                                <Col> */}
+                                    <Button
+                                    type="submit"
+                                    className="mb-3 w-100"
+                                    >
+                                    Enviar
+                                    </Button>
+                                {/* </Col>
+                            </Row> */}
+                            <Link to='/login' className="link-secondary text-decoration-none">Entrar</Link>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
         </>
     )
 }
